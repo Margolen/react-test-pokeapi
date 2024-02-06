f9c8c1524dd1a8803e88aa51557cf8151eb996c6
Margarita Shishkova <margolen239@gmail.com>
2024-02-06 19:20:11 +0000

    Add select for cards

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import CSSModules from "react-css-modules";
 import style from "./style.module.scss";
 
 export const Header = () => {
-  return <div styleName="header">Header</div>;
+  const [isOpen, setOpen] = useState();
+
+  return (
+    <header styleName="header">
+      <a href="#" styleName="header__logo">
+        <img src="/assets/logo.png" alt="home" />
+      </a>
+      <nav styleName={`header__nav ${isOpen ? "active" : ""}`}>
+        <ul styleName="header__nav-list">
+          <li styleName="header__nav-item">Home</li>
+          <li styleName="header__nav-item">About</li>
+          <li styleName="header__nav-item">Contacts</li>
+        </ul>
+      </nav>
+      <button styleName="header__menu-button" onClick={() => setOpen(!isOpen)}>
+        <faBars />
+      </button>
+    </header>
+  );
 };
 
 export default CSSModules(Header, style);
--- src/Pages/app/App.jsx
+++ src/Pages/app/App.jsx
@@ -9,19 +9,63 @@ import style from "./style.module.scss";
 
 export function App() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonLimit, setPokemonLimit] = useState(20);
+  const [pokemonOffset, setPokemonOffset] = useState(0);
+  const [pokemonCount, setPokemonCount] = useState(0);
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) => {
-      console.log(response);
-      setPokemonList(response.data.results);
-    });
-  }, [setPokemonList]);
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/", {
+        params: {
+          limit: pokemonLimit,
+          offset: pokemonOffset,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setPokemonList(response.data.results);
+        setPokemonCount(response.data.count);
+      });
+  }, [setPokemonList, pokemonOffset, pokemonLimit]);
+
+  const clamp = (x, min, max) => {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+  };
 
   return (
     <Router>
       <div styleName="wrapper">
         <Header />
         <main>
+          <button
+            onClick={() =>
+              setPokemonOffset(
+                clamp(pokemonOffset - pokemonLimit, 0, pokemonCount)
+              )
+            }
+          >
+            prev
+          </button>
+          <select
+            value={pokemonLimit}
+            onChange={(e) => {
+              setPokemonLimit(e.target.value);
+            }}
+          >
+            <option value="20">20</option>
+            <option value="30">30</option>
+          </select>
+          <button
+            onClick={() =>
+              setPokemonOffset(
+                clamp(pokemonOffset + pokemonLimit, 0, pokemonCount)
+              )
+            }
+          >
+            next
+          </button>
           <div styleName="card__list">
             {pokemonList.map((pokemon) => (
               <Card key={pokemon.url} url={pokemon.url} name={pokemon.name} />
