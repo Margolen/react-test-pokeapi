695ef7067ee884ba54c7f228b684fa38edfd5e17
Margarita Shishkova <margolen239@gmail.com>
2024-02-07 00:24:22 +0000

    Move App outside from pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import { Home } from "../Home/Home";
+import { Home } from "./Pages/Home/Home";
 
 export function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../src/Pages/app/App";
+import App from "./App";
 import "./index.scss";
 
 const root = document.getElementById("root");
