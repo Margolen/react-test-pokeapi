747b1c713392ac4b9bf078e6d348bfeae3847fe6
Margarita Shishkova <margolen239@gmail.com>
2024-02-07 23:35:36 +0000

    Create footer and style it

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,18 @@
 import styles from "./style.module.scss";
 
 export const Footer = () => {
-  return <div className={styles["footer"]}>Footer</div>;
+  return (
+    <div className={styles["footer"]}>
+      <div className={styles["footer__contact"]}>
+        <p>
+          Made by &nbsp;
+          <a href="https://www.linkedin.com/in/margarita-shishkova/">
+            Margarita Shishkova
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
