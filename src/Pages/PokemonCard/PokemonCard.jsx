215e16d6b50214697afac050c32b52f753f0885b
Margarita Shishkova <margolen239@gmail.com>
2024-02-07 20:27:51 +0000

    Display basic pokemon data on the pokemon page

--- src/Pages/PokemonCard/PokemonCard.jsx
+++ src/Pages/PokemonCard/PokemonCard.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export const PokemonCard = () => {
-  return <div>PokemonCard</div>;
+  const { pokemonId } = useParams();
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    if (pokemonId) {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
+        .then((response) => {
+          console.log(response);
+          setPokemon(response.data);
+        });
+    }
+  }, [pokemonId]);
+
+  return (
+    <div>
+      {pokemon !== null && (
+        <div>
+          <p>{pokemon.id}</p>
+          <p>{pokemon.name}</p>
+          <p>{pokemon.height}</p>
+          <p>{pokemon.weight}</p>
+        </div>
+      )}
+    </div>
+  );
 };
