0e32977c926fa4efd688fc330929bb70c3840306
Margarita Shishkova <margolen239@gmail.com>
2024-02-08 23:13:07 +0000

    Add more stylised skeleton for the cards

--- src/Components/CardSkeleton/CardSkeleton.jsx
+++ src/Components/CardSkeleton/CardSkeleton.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Skeleton from "../Skeleton/Skeleton";
+import styles from "./style.module.scss";
 
 export default function CardSkeleton() {
   return (
-    <Skeleton width={"50%"} padding={"0 5px"} margin={"1em"} height={"300px"} />
+    <div className={styles["card"]}>
+      <div className={styles["card__img"]}>
+        <Skeleton width={"208px"} height={"208px"} />
+      </div>
+      <div className={styles["card__content"]}>
+        <Skeleton width={"10em"} height={"2em"} />
+        <Skeleton width={"9em"} height={"1.5em"} />
+      </div>
+    </div>
   );
 }
