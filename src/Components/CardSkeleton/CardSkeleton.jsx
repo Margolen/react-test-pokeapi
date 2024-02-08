172f104fbbb49aa5917f1aa677cdbed913bcad4d
Margarita Shishkova <margolen239@gmail.com>
2024-02-08 22:53:20 +0000

    Change height for the cards to 300px

--- src/Components/CardSkeleton/CardSkeleton.jsx
+++ src/Components/CardSkeleton/CardSkeleton.jsx
@@ -3,6 +3,6 @@ import Skeleton from "../Skeleton/Skeleton";
 
 export default function CardSkeleton() {
   return (
-    <Skeleton width={"50%"} padding={"0 5px"} margin={"1em"} height={"200px"} />
+    <Skeleton width={"50%"} padding={"0 5px"} margin={"1em"} height={"300px"} />
   );
 }
